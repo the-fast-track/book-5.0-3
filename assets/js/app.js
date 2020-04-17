4351cffd8aa75a423745ec7e145ec1b334a89a66
Fabien Potencier <fabien@potencier.org>
2020-04-17 08:13:22 +0200

    Add 22.2: Styling the HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.scss';
+import 'bootstrap';
+import bsCustomFileInput from 'bs-custom-file-input';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+bsCustomFileInput.init();
